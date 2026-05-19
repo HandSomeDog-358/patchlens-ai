513b65b80e8d43f47886c4f53540aa97f754aa2f
HandSomeDog-358 <374641059@qq.com>
2026-05-19 15:34:07 +0800

    Initial open source release (20/152)

---
+++ backend/src/main/java/com/patchlens/config/PatchLensConfig.java
@@ -0,0 +1,9 @@
+package com.patchlens.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(PatchLensProperties.class)
+public class PatchLensConfig {
+}
